68a09b6c10377f39f471b301dd10486da74f8d8d
Md. Al-Amin <amin15-1951@diu.edu.bd>
2025-02-14 01:45:10 +0600

    feat(lang): localization migrate to `synthetic-package: false`

---
+++ lib/l10n/arb/app_localizations_bn.dart
@@ -0,0 +1,202 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Bengali Bangla (`bn`).
+class AppLocalizationsBn extends AppLocalizations {
+  AppLocalizationsBn([String locale = 'bn']) : super(locale);
+
+  @override
+  String get onboardingTitle => 'আমরা যা করি, তাই আমরা';
+
+  @override
+  String get onboardingSubtitle => 'হাজারো মানুষ ছোট ছোট ধ্যানের জন্য সাইলেন্ট মুন ব্যবহার করছে';
+
+  @override
+  String get login => 'লগইন করুন';
+
+  @override
+  String get welcomeBack => 'ফিরে আসার জন্য স্বাগতম!';
+
+  @override
+  String get continueWithGoogle => 'গুগলের মাধ্যমে চালিয়ে যান';
+
+  @override
+  String get continueWithFacebook => 'ফেসবুকের মাধ্যমে চালিয়ে যান';
+
+  @override
+  String get orLoginWithEmail => 'অথবা ইমেইল দিয়ে লগইন করুন';
+
+  @override
+  String get emailAddress => 'ইমেইল ঠিকানা';
+
+  @override
+  String get password => 'পাসওয়ার্ড';
+
+  @override
+  String get forgetPassword => 'পাসওয়ার্ড ভুলে গেছেন?';
+
+  @override
+  String get alreadyHaveAccount => 'ইতিমধ্যেই একটি অ্যাকাউন্ট আছে?';
+
+  @override
+  String get donNotHaveAccount => 'আপনার কি অ্যাকাউন্ট নেই?';
+
+  @override
+  String get createYourAccount => 'আপনার অ্যাকাউন্ট তৈরি করুন';
+
+  @override
+  String get orSignupWithEmail => 'অথবা ইমেইল দিয়ে সাইন আপ করুন';
+
+  @override
+  String get name => 'নাম';
+
+  @override
+  String get iHaveReadThe => 'আমি পড়েছি';
+
+  @override
+  String get privacyPolicy => 'গোপনীয়তা নীতি';
+
+  @override
+  String get getStarted => 'শুরু করুন';
+
+  @override
+  String get signup => 'নিবন্ধন করুন';
+
+  @override
+  String hiUser(Object name) {
+    return 'হাই $name,';
+  }
+
+  @override
+  String welComeToAppName(Object app_name) {
+    return '$app_name এ স্বাগতম';
+  }
+
+  @override
+  String get welcomeSubtitle => 'অ্যাপটি অন্বেষণ করুন, ধ্যানের জন্য প্রস্তুত হতে মানসিক শান্তি খুঁজুন।';
+
+  @override
+  String get whatBringsYou => 'কি তোমাকে এনেছে';
+
+  @override
+  String toAppName(Object app_name) {
+    return '$app_name এ?';
+  }
+
+  @override
+  String get chooseATopic => 'একটি বিষয় নির্বাচন করুন যাতে মনোযোগ কেন্দ্রীভূত করা যায়:';
+
+  @override
+  String get meditateTimeTitle => 'আপনি কখন মেডিটেশন করতে চান?';
+
+  @override
+  String get meditateTimeSubtitle => 'আপনি যেকোনো সময় বেছে নিতে পারেন, তবে আমরা সকালে প্রথমেই করার পরামর্শ দিই।';
+
+  @override
+  String get meditateDayTitle => 'আপনি কোন দিনে মেডিটেশন করতে চান?';
+
+  @override
+  String get meditateDaySubtitle => 'প্রতিদিন করাই সর্বোত্তম, তবে অন্তত পাঁচ দিন বেছে নেওয়ার পরামর্শ দিই।';
+
+  @override
+  String get save => 'সংরক্ষণ করুন';
+
+  @override
+  String get noThanks => 'না, ধন্যবাদ';
+
+  @override
+  String get home => 'হোম';
+
+  @override
+  String get sleep => 'ঘুম';
+
+  @override
+  String get meditate => 'ধ্যান';
+
+  @override
+  String get music => 'সঙ্গীত';
+
+  @override
+  String get profile => 'প্রোফাইল';
+
+  @override
+  String get exitApp => 'অ্যাপ থেকে বের হবেন';
+
+  @override
+  String get areYouSureToExit => 'আপনি কি নিশ্চিত যে আপনি বের হতে চান?';
+
+  @override
+  String get no => 'না';
+
+  @override
+  String get yes => 'হ্যাঁ';
+
+  @override
+  String goodMorningUser(Object name) {
+    return 'সুপ্রভাত, $name';
+  }
+
+  @override
+  String goodAfternoonUser(Object name) {
+    return 'শুভ অপরাহ্ন, $name';
+  }
+
+  @override
+  String goodEveningUser(Object name) {
+    return 'শুভ সন্ধ্যা, $name';
+  }
+
+  @override
+  String get weWishYouHaveAGoodDay => 'আমরা আশা করি আপনার দিন ভালো কাটবে!';
+
+  @override
+  String get basics => 'মৌলিক বিষয়';
+
+  @override
+  String get course => 'কোর্স';
+
+  @override
+  String minCount(Object minCount) {
+    return '$minCount মিনিট';
+  }
+
+  @override
+  String get relaxation => 'বিশ্রাম';
+
+  @override
+  String get start => 'শুরু করুন';
+
+  @override
+  String get dailyThought => 'দৈনিক চিন্তা';
+
+  @override
+  String get meditation => 'ধ্যান';
+
+  @override
+  String get goodMorning => 'শুভ সকাল';
+
+  @override
+  String get courseDetails => 'গভীর, পরিবেষ্টিত সুরগুলির মাধ্যমে মনকে আরামদায়ক ঘুমের জন্য প্রস্তুত করুন।';
+
+  @override
+  String favoriteCount(Object count) {
+    return '$count পছন্দ';
+  }
+
+  @override
+  String listenCount(Object count) {
+    return '$count শুনছে';
+  }
+
+  @override
+  String get pickANarrator => 'একজন বক্তা নির্বাচন করুন';
+
+  @override
+  String get maleVoice => 'পুরুষ কণ্ঠ';
+
+  @override
+  String get femaleVoice => 'মহিলা কণ্ঠ';
+}
